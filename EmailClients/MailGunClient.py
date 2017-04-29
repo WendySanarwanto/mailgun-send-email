a63c9ee1da4544c6d28a085e1c3a71915f62a877
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-04-29 19:53:13 +0800

    Added comments on MailGunClient class

--- EmailClients/MailGunClient.py
+++ EmailClients/MailGunClient.py
@@ -1,13 +1,22 @@
 import requests
 
+
 class MailGunClient:
+    """
+        Provide methods for accessing mailing services provided by MailGun (https://www.mailgun.com/)
+    """
+
     def __init__(self, domain, api_key):
+        """ Constructor of 'MailGunClient' class"""
         self.api_url = "https://api.mailgun.net/v3/%s" % domain
         print "api_url = %s" % self.api_url
         self.messages_resource_path = "/messages"
         self.api_key = api_key
 
     def send_mail(self, _from, to, subject, text):
+        """
+            Compose email message by specified 'from' email address, recipient's email address(es), subject and text
+        """
         return requests.post(
             self.api_url + self.messages_resource_path, 
             auth=("api", self.api_key),
